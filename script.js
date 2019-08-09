daf0268be104e61ee84ca619623621c8547d132b
Sid Roberts <sid@sidroberts.co.uk>
2019-08-09 12:07:31 +0900

    Tidied up logic

--- script.js
+++ script.js
@@ -62,9 +62,11 @@ $(document).ready(
                 keyCode = (e.keyCode ? e.keyCode : e.which);
 
                 // F keys
-                if (keyCode < 112 || keyCode > 123) {
-                    writeRandomName();
+                if (keyCode >= 112 && keyCode <= 123) {
+                    return;
                 }
+
+                writeRandomName();
             }
         );
 
